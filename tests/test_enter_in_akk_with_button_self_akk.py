4a2fcbd665f71ab6134da492510677b75b177dff
PSIHboogaga <166983286+PSIHboogaga@users.noreply.github.com>
2024-05-13 19:54:58 +0500

    Add files via upload

---
+++ tests/test_enter_in_akk_with_button_self_akk.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from fixture import driver
+
+class TestRegistration:
+    def test_enter_in_akk_with_button_self_akk(self, driver):
+        driver.find_element(By.XPATH, "//div/header/nav/a/p").click()  # Клик кнопки личный кабинет
+        WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.XPATH, "//div/main/div/form/fieldset[1]/div/div/input")))
+        driver.find_element(By.XPATH, "//div/main/div/form/fieldset[1]/div/div/input").send_keys("IvanMakarov5555@mail.ru")  # ввод email
+        driver.find_element(By.NAME, "Пароль").send_keys("1q2w3e4r5t")  # ввдод пароля
+        driver.find_element(By.XPATH, "//div/main/div/form/button").click() # Клик кнопки войти
+        WebDriverWait(driver, 10).until(
+            expected_conditions.element_to_be_clickable((By.XPATH, "//div/header/nav/a/p")))
+        driver.find_element(By.XPATH, "//div/header/nav/a/p").click() # Клик кнопки личный кабинет
+        WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, "//div/main/div")))
+        assert driver.find_element(By.XPATH, "//div/main/div").is_displayed()  #
